6ed9d1dcacc41728604b4e3c8441a9382af3231a
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2025-12-19 17:28:54 +0000

    refactor

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,5 +1,5 @@
 import pytest
-from typing import Union, List
+from typing import List
 from app.split_integer import split_integer
 
 
@@ -8,45 +8,30 @@ class TestSplitInteger:
         "value, number_of_parts, expected",
         [
             pytest.param(
-                8,
-                1,
-                [8],
-                id="Should be list instance"
+                8, 1, [8],
+                id="should_split_into_one_part"
             ),
             pytest.param(
-                6,
-                2,
-                [3, 3],
-                id="Length of the parts is equal to number_of_parts"
+                6, 2, [3, 3],
+                id="should_split_into_equal_parts"
             ),
             pytest.param(
-                17,
-                4,
-                [4, 4, 4, 5],
-                id="Should be sorted when not equal"
+                17, 4, [4, 4, 4, 5],
+                id="should_be_sorted_and_differ_by_maximum_one"
             ),
             pytest.param(
-                32,
-                6,
-                [5, 5, 5, 5, 6, 6],
-                id="Should be above 0"
+                32, 6, [5, 5, 5, 5, 6, 6],
+                id="should_handle_large_numbers_correctly"
             ),
             pytest.param(
-                21,
-                4,
-                [5, 5, 5, 6],
-                id="Sum of the parts should be equal to value"
+                21, 4, [5, 5, 5, 6],
+                id="sum_of_parts_should_equal_value"
             )
         ])
     def test_split_integer_is_working_properly(
             self,
-            value: Union[int, float],
-            number_of_parts: Union[int, float],
-            expected: List[Union[int, float]]
+            value: int,
+            number_of_parts: int,
+            expected: List[int]
     ) -> None:
-        parts = split_integer(value, number_of_parts)
-        assert isinstance(parts, list)
-        assert len(parts) == number_of_parts
-        assert parts == sorted(parts)
-        assert max(parts) - min(parts) <= 1
-        assert sum(parts) == value
+        assert split_integer(value, number_of_parts) == expected
